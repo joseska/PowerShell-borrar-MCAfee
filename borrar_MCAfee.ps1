ef221dd7ef3f90b647319f22894e9d5de447115b
joseska <joseska@gmail.com>
2017-05-27 22:31:39 +0100

    Añadir archivo
    añadir archivo

---
+++ borrar_MCAfee.ps1
@@ -0,0 +1,6 @@
+﻿#Primero tenemos que activar el Powershell Remoting. Sino el Invoke-Command no funciona
+Enable-PSRemoting -Force
+
+
+$commando = {ping Joseska-PC}
+Invoke-Command -scriptblock $commando -computername "Joseska-PC"
