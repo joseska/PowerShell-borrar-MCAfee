131310ef819378ccaeb402916730a408546b4e54
joseska <joseska@gmail.com>
2017-06-09 13:08:46 +0100

    Cambios 3
    eso mismo

--- borrar_MCAfee.ps1
+++ borrar_MCAfee.ps1
@@ -1,18 +1,23 @@
 ﻿# ejecutar Script en Equipos con el "Set-ExecutionPolicy restricted"
 # Set-ExecutionPolicy Unrestricted
 
-# Borramos el MCAffe Enterprise y el Agent
+
+# Borramos el MCAffe Enterprise
 $Programa = Get-WmiObject -Class Win32_Product -Filter "Name = 'McAfee VirusScan Enterprise'"
-$Programa.Uninstall()
-
-$uninstall64 = (Get-ItemProperty HKLM:\SOFTWARE\Wow6432Node\McAfee\Agent -ErrorAction SilentlyContinue).InstallPath
-$uninstall32 = (Get-ItemProperty HKLM:\SOFTWARE\McAfee\Agent -ErrorAction SilentlyContinue).InstallPath
-
+if ($Programa -eq $False) {
+    $Programa.Uninstall()
+}
+else {
+    write-host "No está instalado el McAfee VirusScan Enterprise"
+}
 
 Start-Sleep -s 3
 
+# Borramos el MCAffe Agent
+$uninstall64 = (Get-ItemProperty HKLM:\SOFTWARE\Wow6432Node\McAfee\Agent -ErrorAction SilentlyContinue).InstallPath
+$uninstall32 = (Get-ItemProperty HKLM:\SOFTWARE\McAfee\Agent -ErrorAction SilentlyContinue).InstallPath
 
-if ($uninstall64) {
+if ($uninstall64 -eq $False) {
     $uninstall64 =  "$($uninstall64)frminst.exe"
     $uninstall64 = $uninstall64.Trim()
 
@@ -21,16 +26,18 @@ if ($uninstall64) {
     Write "desinstalando 64Bits /forceuninstall. Puede tardar bastante..."
     Start-Process -FilePath $uninstall64 -args "/forceuninstall /Silent" -Wait
 }
-
-else {
-    $uninstall32 =  "$($uninstall32)frminst.exe"
-    $uninstall32 = $uninstall32.Trim()
-
-    Write "desinstalando 32Bits /remove=agent"
+ElseIf ($uninstall32 -eq $False) {
+    Write "desinstalando 32Bits /remove=agent. Puede tardar bastante..."
     Start-Process -FilePath $uninstall32 -args "/remove=agent" -Wait
-    Write "desinstalando 32Bits /forceuninstall"
+    Write "desinstalando 32Bits /forceuninstall. Puede tardar bastante..."
     Start-Process -FilePath $uninstall32 -args "/forceuninstall" -Wait
 }
 
+else {
+    Write "No está instalado el MCAfee Agent"
+}
+
+
+
 # habilitar de nuevo "Set-ExecutionPolicy restricted"
-# Set-ExecutionPolicy restricted
+# Set-ExecutionPolicy undefined
